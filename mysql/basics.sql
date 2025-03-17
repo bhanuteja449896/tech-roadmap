951932732b03fe3b28ca7c8af4f63ab4e32306ee
bhanuteja449896 <bhanutejamakkineni@gmail.com>
2025-03-18 01:03:55 +0530

    executed some basic sql commands and saved their outputs in same file under their respective commands

--- mysql/basics.sql
+++ mysql/basics.sql
@@ -1,4 +1,42 @@
+-- select is used to select the data from the table in a database
 select * from example.student;
+
+-- id	name	age	grade	email
+-- 1	bhanu	21	A	bhanu@gmail.com
+-- 3	bhanueja	21	A	bhanuteja@gmail.com
+-- 4	teja	21	A	teja@gmail.com
+-- 5	bhanu	20	B	bt@gmail.com
+
+
+
+-- distinct is used to select the data from the table in a database but removing duplicates like set in python 
+
 select distinct name from student;
+-- name
+-- bhanu
+-- bhanueja
+-- teja
+
+
 select distinct age from student;
 
+-- age
+-- 21
+-- 20
+
+
+
+
+-- where caluse
+-- it is used to filter the records based on the conditions
+
+select * from student where age= 20;
+-- id	name	age	grade	email
+-- 5	bhanu	20	B	bt@gmail.com
+
+
+select * from student where grade = 'A';
+-- id	name	age	grade	email
+-- 1	bhanu	21	A	bhanu@gmail.com
+-- 3	bhanueja	21	A	bhanuteja@gmail.com
+-- 4	teja	21	A	teja@gmail.com
